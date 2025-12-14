55f0ae243edace7a551cd360660552216895d10b
GustavoMacielLima <gustavomacielreallima@gmail.com>
2025-12-14 15:08:53 -0300

    Fix enviroment config

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,7 @@
-import { Environment } from './environment';
-
-export const environment: Environment = {
+export const environment:  {
+  production: boolean;
+  apiUrl: string;
+} = {
   production: false,
   apiUrl: 'https://jsonplaceholder.typicode.com'
 };
